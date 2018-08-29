c86fdf17b2cb5b91c3d885c0512c0f93cffd6a8e
Titus Tienaah <titus.intdxdt@gmail.com>
2018-08-29 18:59:51 -0300

    args refs

--- cmap.go
+++ cmap.go
@@ -1,51 +1,65 @@
-package x_cmap
+package cmap
 
-import "sync"
+import (
+	"sync"
+	"github.com/TopoSimplify/node"
+	"sort"
+)
 
-//@formatter:off
+const CacheKeySize = 6
 
-type Map struct {
-    sync.RWMutex
-    dict map[string]struct{}
+type CacheMap struct {
+	sync.RWMutex
+	dict map[[CacheKeySize]int]struct{}
 }
 
-func NewMap() *Map {
-    return &Map{dict:make(map[string]struct{})}
+func NewCacheMap(size int) *CacheMap {
+	return &CacheMap{dict: make(map[[CacheKeySize]int]struct{}, size)}
 }
 
-func (m *Map) HasKey(key string) bool {
-    m.RLock()
-    var _, ok = m.dict[key]
-    m.RUnlock()
-    return ok
+func (m *CacheMap) HasKey(key [CacheKeySize]int) bool {
+	m.RLock()
+	var _, ok = m.dict[key]
+	m.RUnlock()
+	return ok
 }
 
-func (m *Map) Set(key string) {
-    m.Lock()
-    m.dict[key] = struct{}{}
-    m.Unlock()
+func (m *CacheMap) Set(key [CacheKeySize]int) {
+	m.Lock()
+	m.dict[key] = struct{}{}
+	m.Unlock()
 }
 
-func (m *Map) Delete(key string) {
-    m.Lock()
-    delete(m.dict, key)
-    m.Unlock()
+func (m *CacheMap) Delete(key [CacheKeySize]int) {
+	m.Lock()
+	delete(m.dict, key)
+	m.Unlock()
 }
 
-func (m *Map) Size() int {
-    m.RLock()
-    var v = len(m.dict)
-    m.RUnlock()
-    return v
+func (m *CacheMap) Size() int {
+	m.RLock()
+	var v = len(m.dict)
+	m.RUnlock()
+	return v
 }
 
-
-func (m *Map) Keys () []string {
-    m.Lock()
-    var keys = make([]string , m.Size())
-    for k := range m.dict {
-        keys = append(keys, k)
-    }
-    m.Unlock()
-    return keys
+func (m *CacheMap) Keys() [][CacheKeySize]int {
+	m.RLock()
+	var keys = make([][CacheKeySize]int, m.Size())
+	for k := range m.dict {
+		keys = append(keys, k)
+	}
+	m.RUnlock()
+	return keys
 }
+
+func CacheKey(a, b *node.Node) [CacheKeySize]int {
+	var o = [CacheKeySize]int{
+		a.Range.I, a.Range.J,
+		b.Range.I, b.Range.J,
+		a.Instance.Id(), b.Instance.Id(),
+	}
+	sort.Ints(o[:])
+	return o
+}
+
